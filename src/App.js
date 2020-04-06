1c5aed5cd42d38108041367b0f51c3ab3958103b
Brian Sunday <brians931@gmail.com>
2020-04-06 20:23:19 +0300

    install axios to fetch users from the github api and pass the new user array to the users component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React, { Component, Fragment } from "react";
 import NavbarComponent from "../src/components/layouts/NavbarComponent";
 import UsersComponent from "./components/users/UsersComponent";
+import axios from "axios";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: true,
+  };
+
+  async componentDidMount() {
+    // make github request
+    const res = await axios.get("https://api.github.com/users");
+
+    this.setState({ user: res.data, loading: false });
+  }
+
   render() {
     return (
       <div className="App">
         <NavbarComponent title="Github Search" />
         <section className="container mt-2">
-          <UsersComponent />
+          <UsersComponent
+            loading={this.state.loading}
+            users={this.state.users}
+          />
         </section>
       </div>
     );
--- src/components/users/UserItemComponent.js
+++ src/components/users/UserItemComponent.js
@@ -1,25 +1,26 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-export class UserItemComponent extends Component {
-  render() {
-    const { login, avatar_url, html_url } = this.props.user;
-
-    return (
-      <div className="card text-center col-md-4 ">
-        <img src={avatar_url} className="card-img-top" alt="" />
-        <h3>{login}</h3>
-        <div>
-          <a
-            href={html_url}
-            rel="nooponer norefer"
-            className="btn btn-primary btn-sm"
-          >
-            More
-          </a>
-        </div>
+const UserItemComponent = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className="card text-center col-md-4 ">
+      <img src={avatar_url} className="card-img-top" alt="" />
+      <h3>{login}</h3>
+      <div>
+        <a
+          href={html_url}
+          rel="nooponer norefer"
+          className="btn btn-primary btn-sm"
+        >
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+UserItemComponent.propTypes = {
+  user: PropTypes.object.isRequired,
+};
 
 export default UserItemComponent;
