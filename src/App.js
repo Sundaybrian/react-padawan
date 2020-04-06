d7294d1efcc8ae3bdbe12fa38b5efe067a27741f
Brian Sunday <brians931@gmail.com>
2020-04-06 16:09:24 +0300

    experimenting with jsx expressions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import "./App.css";
 
 class App extends Component {
   render() {
+    const name = "Brian Sunday";
+    const loading = false;
+    const showName = true;
+
     return (
       <div className="App">
-        <h1>Hello React</h1>
+        {loading ? <h4>Loading....</h4> : <h1>{showName && name}</h1>}
       </div>
     );
   }
